a14c082a8a730d7e10f9eeb99711e104ad3818ca
wesleyfriedman <friedman.wesley@gmail.com>
2016-10-10 21:50:04 -0400

    Done

---
+++ app/models/show.rb
@@ -0,0 +1,31 @@
+class Show < ActiveRecord::Base
+
+	def self.highest_rating
+		self.order(rating: :desc).first.rating
+	end
+
+	def self.most_popular_show
+		self.order(rating: :desc).first
+	end
+
+	def self.lowest_rating
+		self.order(rating: :asc).first.rating
+	end
+
+	def self.least_popular_show
+		self.order(rating: :asc).first
+	end
+
+	def self.ratings_sum
+		self.sum(:rating)
+	end
+
+	def self.popular_shows
+		self.where("rating > 5")
+	end
+
+	def self.shows_by_alphabetical_order
+		self.order(:name)
+	end
+
+end
